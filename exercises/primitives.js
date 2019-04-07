f8c2af9f04d735cb9e38dc09a8516bf3b51aeefa
Candice Bourguignon <candice.bourguignon@gmail.com>
2019-04-07 17:35:02 +0200

    basics training

--- exercises/primitives.js
+++ exercises/primitives.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 /*
  * Create a constant variables for each primitives
@@ -6,22 +6,19 @@
  *
  * @notions Primitive and Operators, Variables
  */
-
 // Your code :
-
-
 //* Begin of tests
-const assert = require('assert')
+const str = '1337', num = 42, bool = false, undef = undefined, assert = require('assert');
 
-assert.strictEqual(typeof str, 'string')
-assert.strictEqual(str, '1337')
+assert.strictEqual(typeof str, 'string');
+assert.strictEqual(str, '1337');
 
-assert.strictEqual(typeof num, 'number')
-assert.strictEqual(num, 42)
+assert.strictEqual(typeof num, 'number');
+assert.strictEqual(num, 42);
 
-assert.strictEqual(typeof bool, 'boolean')
-assert.strictEqual(bool, false)
+assert.strictEqual(typeof bool, 'boolean');
+assert.strictEqual(bool, false);
 
-assert.strictEqual(typeof undef, 'undefined')
-assert.strictEqual(undef, undefined)
+assert.strictEqual(typeof undef, 'undefined');
+assert.strictEqual(undef, undefined);
 // End of tests */
